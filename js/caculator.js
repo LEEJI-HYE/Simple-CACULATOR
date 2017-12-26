013b4f3f65bcfbf5f2e8d2056f1819bc8e2bfae1
LEEJIHYE <onlyjh0924@gmail.com>
2017-12-26 15:13:39 +0900

    내용추가

--- js/caculator.js
+++ js/caculator.js
@@ -16,16 +16,13 @@
    	}
 
    function caculator(){
-   	    var inputTag1 = getValueOfTag('input1');
-   	    var inputTag2 = getValueOfTag('input2');
+   	    var inputTag1 = getValueOfTag('caculator1');
+   	    var inputTag2 = getValueOfTag('caculator2');
   		// var inputTag1 = Number(document.getElementById('caculator1').value);
   		// var inputTag2 = Number(document.getElementById('caculator2').value);
-
-  		var target = document.getElementById('operators');
-
-  		var op = getOptionValue(target);
+  		
   		// var op = target.options[target.selectedUndex].value;
-  		var numOp = Number(op);
+  		var numOp =getOptionValue('operators');
 
   		console.log(inputTag1);
   		console.log(inputTag2);
@@ -34,6 +31,7 @@
   		var result = 9999999999999999;
   		if(numOp == 1){
   			result = puls(inputTag1,inputTag2);
+  			console.log("result in if : " + result);
   		} else if (numOp == 2){
   			result = minus(inputTag1,inputTag2);
 			//  result = inputTag1 - inputTag2;
@@ -41,7 +39,7 @@
   			result = multiple(inputTag1,inputTag2);
   			//  result = inputTag1 * inputTAg2;
   		} else if (numOp == 4){
-  			result = division(diinputTag1,inputTag2);
+  			result = division(-inputTag1,inputTag2);
   			//  result = inputTag1 / inputTag2;
   		}
   		printResult('result',result);
